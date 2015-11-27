8c8c82b565b8716179f1e92d94ac7da2c6f53a27
spyshiv <spyshiv@gmail.com>
2015-11-27 10:40:01 +0530

    truncatejs added

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,9 @@
  $(".button-collapse").sideNav();
+   $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+ 
+  $('.truncate_words').truncate({
+      lines: 2,
+      lineHeight: 20
+    });
